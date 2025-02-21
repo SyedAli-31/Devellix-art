63487588f338a3902f4c6bbcd692ae48ea550054
SyedAli-31 <aligarians4657@gmail.com>
2025-02-22 00:18:36 +0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors
+  },
+  experimental: {
+    forceSwcTransforms: true, // Use SWC for performance boost
+  },
 };
 
 export default nextConfig;
